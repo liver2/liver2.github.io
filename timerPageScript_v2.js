5da0472381922b64697b6128df4a86e375ad8bf0
liver2 <124078178+liver2@users.noreply.github.com>
2023-07-16 16:07:33 -0400

    Update timerPageScript_v2.js

--- timerPageScript_v2.js
+++ timerPageScript_v2.js
@@ -20,8 +20,7 @@ var timerFinishedAudio = new Audio("kinderszenenRingtone.mp3"); // New audio for
 
 document.getElementById("focusBlockButton").style.textDecoration = "underline";
 
-document.querySelector('input[id="minuteFieldInput"]').style.textDecoration = "underline";
-document.querySelector('input[id="secondFieldInput"]').style.textDecoration = "underline";
+document.getElementById('clickme').style.visibility = "visible";
 
 document.querySelector('input[id="minuteFieldInput"]').value = dm.toString();
 document.querySelector('input[id="secondFieldInput"]').value = 0 + ds.toString();
@@ -39,9 +38,8 @@ function tabSwitch() {
     document.querySelector('input[id="minuteFieldInput"]').disabled = false;
     document.querySelector('input[id="secondFieldInput"]').disabled = false;
 
-    document.querySelector('input[id="minuteFieldInput"]').style.textDecoration = "underline";
-    document.querySelector('input[id="secondFieldInput"]').style.textDecoration = "underline";
-
+    document.getElementById('clickme').style.visibility = "visible";
+    
     document.getElementById("playButton").innerHTML = "PLAY";
     document.getElementById("pauseButton").innerHTML = "";
     document.getElementById("resumeButton").innerHTML = "";
@@ -57,8 +55,7 @@ function playResume() {
         document.querySelector('input[id="minuteFieldInput"]').disabled = true;
         document.querySelector('input[id="secondFieldInput"]').disabled = true;
 
-        document.querySelector('input[id="minuteFieldInput"]').style.textDecoration = "none";
-        document.querySelector('input[id="secondFieldInput"]').style.textDecoration = "none";
+        document.getElementById('clickme').style.visibility = "hidden";
 
         document.getElementById("playButton").style.opacity = 0;
         document.getElementById("pauseButton").style.opacity = 100;
