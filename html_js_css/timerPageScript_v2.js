f7fde7dea2e32d77c2e9e6d78eb077c1c5d2f86f
liver2 <124078178+liver2@users.noreply.github.com>
2023-08-07 11:39:48 -0400

    HUGE UPDATE restructuring folders, bell, click snd

--- html_js_css/timerPageScript_v2.js
+++ html_js_css/timerPageScript_v2.js
@@ -14,9 +14,9 @@ var timerStatus = "NOT_STARTED"; // other options: "TICKING", "PAUSED", "FINISHE
 
 var timerInterval; // Declaring this variable now so that it's global
 
-var timerFinishedAudio = new Audio("kinderszenenRingtone.mp3"); // New audio for timer finished
+var timerFinishedAudio = new Audio("../sounds/bell.mp3"); // New audio for timer finished
 
-var clickAudio = new Audio("Mouse-Click-00-m-FesliyanStudios.com.mp3"); 
+var clickAudio = new Audio("../sounds/click.mp3"); 
 
 // --------------
 
